2eaf0f7f55487f8306b59eae1c2898c9e5cdfabe
maryvonneanee7n <maryvonneanee7n@gmx.com>
2025-03-04 16:09:32 +0000

    Add 1.js

---
+++ src/1.js
@@ -0,0 +1,27 @@
+// Math Problem 1
+const problem1 = () => {
+  const x = Math.floor(Math.random() * 10) + 1;
+  const y = Math.floor(Math.random() * 10) + 1;
+  return `What is the value of ${x} + ${y}?`;
+};
+
+// Math Problem 2
+const problem2 = () => {
+  const x = Math.floor(Math.random() * 10) + 1;
+  const y = Math.floor(Math.random() * 10) + 1;
+  return `What is the value of ${x} - ${y}?`;
+};
+
+// Math Problem 3
+const problem3 = () => {
+  const x = Math.floor(Math.random() * 10) + 1;
+  const y = Math.floor(Math.random() * 10) + 1;
+  return `What is the value of ${x} * ${y}?`;
+};
+
+// Math Problem 4
+const problem4 = () => {
+  const x = Math.floor(Math.random() * 10) + 1;
+  const y = Math.floor(Math.random() * 10) + 1;
+  return `What is the value of ${x} / ${y}?`;
+};
